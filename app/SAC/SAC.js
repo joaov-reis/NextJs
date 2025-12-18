b998fa01d06493e4afc650208f92100669072504
reisjoaov <joaoreisvictor@hotmail.com>
2025-12-18 15:44:23 -0300

    formulário de SAC ajustado

--- app/SAC/SAC.js
+++ app/SAC/SAC.js
@@ -2,7 +2,18 @@
 
 export async function gravarSAC(estadoAnterior, formData) {
     const nome = formData.get("nome");
-    const descricao = formData.get("descricao");
-    console.log(nome + " - " + descricao);
-    return { mensagem: `${nome} gravado com sucesso`, nome: "", descricao: "" };
-}
+    const email = formData.get("email");
+    const telefone = formData.get("telefone");
+    const assunto = formData.get("assunto");
+    const mensagem = formData.get("mensagem");
+    
+    console.log(nome + " - " + email);
+
+    return {
+        confirmacao: "Enviado com sucesso",
+        nome: "",
+        email: "",
+        telefone: "",
+        assunto: "",
+        mensagem: ""
+     };}
