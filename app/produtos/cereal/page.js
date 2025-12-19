d19405981c4741b14773eb61626458d47ff347d7
reisjoaov <joaoreisvictor@hotmail.com>
2025-12-19 02:56:57 -0300

    mais um teste

--- app/produtos/cereal/page.js
+++ app/produtos/cereal/page.js
@@ -1,8 +1,26 @@
-import Image from "next/image";
+// import { GraphQLClient } from "graphql-request";
+// import Image from "next/image";
+// export const client = new GraphQLClient("http://localhost:3000/api/graphql");
 
+// export default async function GraphQLPage() {
+//     const query = `
+//         query {
+//             produtos{
+//                 id
+//                 title
+//                 body
+//             }
+//         }
+//     `;
+
+//     const data = await client.request(query);
+import Image from "next/image";
 export const dynamic = "force-dynamic";
 
-const GRAPHQL_URL = process.env.NEXT_PUBLIC_GRAPHQL_URL;
+const GRAPHQL_URL =
+    process.env.VERCEL
+        ? "http://localhost/api/graphql"
+        : "http://localhost:3000/api/graphql";
 
 export default async function GraphQLPage() {
     const query = `
@@ -22,15 +40,32 @@ export default async function GraphQLPage() {
         cache: "no-store",
     });
 
-    const json = await response.json();
+    const { data } = await response.json();
+
+    const produtoId = "1"; // ID do post que você quer
+    const produto = data.produtos.find((p) => p.id === produtoId);
 
     return (
-        <main style={{ padding: 40 }}>
-            <h1>DEBUG GRAPHQL</h1>
-
-            <pre style={{ textAlign: "left" }}>
-                {JSON.stringify(json, null, 2)}
-            </pre>
+        <main className="bg-[#f5f5dc] text-[#5e503f] min-h-screen flex-col text-center p-12">
+            {produto ? (
+                <div>
+                    <h2 className="font-bold text-lg">{produto.title}</h2>
+                    <p>{produto.body}</p>
+                    <Image
+                        className="rounded-lg mx-auto"
+                        src="/d-l-samuels-68BiDMSe45U-unsplash.jpg"
+                        alt="Cereal-Matinal"
+                        width={500}
+                        height={300}
+                    />
+                    <figcaption> Foto por <a href="https://unsplash.com/photos/a-bowl-full-of-dog-treats-on-a-red-surface-68BiDMSe45U" target="_blank" rel="noopener noreferrer">
+                        D. L. Samuels
+                    </a>
+                    </figcaption>
+                </div>
+            ) : (
+                <p>Post não encontrado</p>
+            )}
         </main>
     );
 }
